fb84a5fdf1b4116a289575631ce59e9f6f79e01d
Rafa_Khadafi <rafa.engineer.dev@gmail.com>
2026-06-30 21:40:14 +0700

    sdsd

--- test.py
+++ test.py
@@ -992,6 +992,8 @@ def footer_page(canvas, doc):
 # tuty
 # tuty
 # tuty
+# tuty
+# tuty
 # ── BUILD PDF ────────────────────────────────────────────────────────────────
 output_path = "Panduan_Belajar_TKA_SMK_2026.pdf"
 doc = SimpleDocTemplate(
